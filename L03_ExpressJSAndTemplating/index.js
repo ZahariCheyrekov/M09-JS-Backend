0a003a97749f7328294ca1069c25de50e3247a14
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 18:58:41 +0300

    Edit app put cats request function

--- L03_ExpressJSAndTemplating/index.js
+++ L03_ExpressJSAndTemplating/index.js
@@ -9,11 +9,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cats', (req, res) => {
-    if (cats.length === 0) {
+    if (cats.length > 0) {
         res.send('No cats :(');
     }
 
     res.send(cats.join(', '));
+});
+
+app.post('/cats', (req, res) => {
+    //TODO: Implement posting cat function logic
+
+    res.send('Add new cat to the collection.');
+});
+
+app.put('/cats', (req, res) => {
+    //TODO: Implement modify cat request function
+
+    res.send('Modify existing cat');
 });
 
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
