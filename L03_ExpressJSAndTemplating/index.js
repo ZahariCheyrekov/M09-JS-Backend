27bbb801472dd98ed13d67543f9fc80be44aaa86
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 21:27:35 +0300

    Edit handlebars layouts

--- L03_ExpressJSAndTemplating/index.js
+++ L03_ExpressJSAndTemplating/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
 
 app.use('/static', express.static('public'));
 
