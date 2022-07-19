152cdeb854aa4e724a6642ec2c3d72106254f890
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 16:57:51 +0300

    Add gzip pipe between read stream and write stream

--- L02_Node.jsStreamsAndUtilities/streams/demo2.js
+++ L02_Node.jsStreamsAndUtilities/streams/demo2.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const zlib = require('zlib');
+
+const gzip = zlib.createGzip();
 
 const readStream = fs.createReadStream('largeFile.txt', { encoding: 'utf-8' });
 const writeStream = fs.createWriteStream('copy.txt', { encoding: 'utf-8' });
 
-readStream.pipe(writeStream);
+// readStream.pipe(writeStream);
+readStream.pipe(gzip).pipe(writeStream);
