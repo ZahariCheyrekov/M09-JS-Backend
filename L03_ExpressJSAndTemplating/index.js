030392c4a01d599ac24a16d8dd802f6c9fcb0b20
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 18:49:25 +0300

    Add get cats function

--- L03_ExpressJSAndTemplating/index.js
+++ L03_ExpressJSAndTemplating/index.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const cats = ['Tom, Garfield, Lin'];
+
 app.get('/', (req, res) => {
-    res.write('Hello, World!');
-    res.end();
+    res.send('Hello, World!');
+});
+
+app.get('/cats', (req, res) => {
+    if (cats.length === 0) {
+        res.send('No cats :(');
+    }
+
+    res.send(cats.join(', '));
 });
 
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
