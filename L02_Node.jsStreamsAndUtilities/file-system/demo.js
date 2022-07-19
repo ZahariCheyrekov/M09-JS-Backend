c5e81a1479174aab430455a80b6af6f692b4fd74
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 17:08:42 +0300

    Add fs read file function

---
+++ L02_Node.jsStreamsAndUtilities/file-system/demo.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// const text = fs.readFileSync('./L02_Node.jsStreamsAndUtilities/file-system/text.txt', { encoding: 'utf-8' });
+fs.readFile(
+    './L02_Node.jsStreamsAndUtilities/file-system/text.txt', { encoding: 'utf-8' },
+    (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log(data);
+    });
