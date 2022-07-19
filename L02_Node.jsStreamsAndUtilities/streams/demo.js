2d3382a3b473a68e72d907dc32c7325521b00919
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 16:49:10 +0300

    Add write stream write logic

--- L02_Node.jsStreamsAndUtilities/streams/demo.js
+++ L02_Node.jsStreamsAndUtilities/streams/demo.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('largeFile.txt', { encoding: 'utf-8' });
-const writeStream = fs.createWriteStream();
+const writeStream = fs.createWriteStream('copy.txt', { encoding: 'utf-8' });
 
 readStream.on('data', (chunk) => {
     console.log(chunk);
@@ -10,3 +10,8 @@ readStream.on('data', (chunk) => {
 readStream.on('end', () => {
     console.log('Finished!');
 });
+
+writeStream.on('finished', () => console.log('Write stream finished!'));
+
+writeStream.write('Hello world!');
+writeStream.end();
