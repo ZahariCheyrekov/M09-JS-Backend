c52795bdf047af6472a078df2a25551997216036
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 18:42:44 +0300

    Config express server

--- L03_ExpressJSAndTemplating/index.js
+++ L03_ExpressJSAndTemplating/index.js
@@ -2,3 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.write('Hello, World!');
+    res.end();
+});
+
+app.listen(5000, () => console.log('Server is listening on port 5000...'));
