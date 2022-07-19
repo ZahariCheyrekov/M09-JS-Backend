eeba71b88a5edf2dffacda802e31c12cef0190e6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 16:02:03 +0300

    Add eventBus publish function declarations

--- L02_Node.jsStreamsAndUtilities/pubsub/demo.js
+++ L02_Node.jsStreamsAndUtilities/pubsub/demo.js
@@ -1,7 +1,9 @@
 const eventBus = require('./eventBus');
 
-eventBus.subscribe('Say hello', (name) => console.log('event say hello executed ' + name));
-eventBus.subscribe('Say hello', (name) => console.log('event say hello second time ' + name));
-eventBus.subscribe('Say bye', (name) => console.log('event say bye executed ' + name));
+eventBus.subscribe('Say hello', (name) => console.log('event say hello executed  - ' + name));
+eventBus.subscribe('Say hello', (name) => console.log('event say hello second time - ' + name));
+eventBus.subscribe('Say bye', (name) => console.log('event say bye executed - ' + name));
 
 eventBus.publish('Say hello', 'Pesho');
+eventBus.publish('Say hello', 'Gosho');
+eventBus.publish('Say bye', 'Mosho');
