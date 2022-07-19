34da9dc37d20e4f2cc0e4f526e3c5cae7785cc0a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 16:40:49 +0300

    Add read stream end function

--- L02_Node.jsStreamsAndUtilities/streams/demo.js
+++ L02_Node.jsStreamsAndUtilities/streams/demo.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('largeFile.txt');
+const readStream = fs.createReadStream('largeFile.txt', { encoding: 'utf-8' });
 
 readStream.on('data', (chunk) => {
     console.log(chunk);
+});
+
+readStream.on('end', () => {
+    console.log('Finished!');
 });
