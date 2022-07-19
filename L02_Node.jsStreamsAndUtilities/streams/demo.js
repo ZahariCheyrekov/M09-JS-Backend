f9e3b51529aca51bd148771e5d9574fb38b937f6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 16:43:51 +0300

    Add copy file

--- L02_Node.jsStreamsAndUtilities/streams/demo.js
+++ L02_Node.jsStreamsAndUtilities/streams/demo.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('largeFile.txt', { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream();
 
 readStream.on('data', (chunk) => {
     console.log(chunk);
