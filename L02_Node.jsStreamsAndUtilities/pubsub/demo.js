0a470e05d754d63685e4512077c62bba035d28f6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 15:54:29 +0300

    Add eventBus subscribe functions

--- L02_Node.jsStreamsAndUtilities/pubsub/demo.js
+++ L02_Node.jsStreamsAndUtilities/pubsub/demo.js
@@ -1,2 +1,5 @@
 const eventBus = require('./eventBus');
 
+eventBus.subscribe('Say hello', () => console.log('event say hello executed'));
+eventBus.subscribe('Say hello', () => console.log('event say hello second time'));
+eventBus.subscribe('Say bye', () => console.log('event say bye executed'));
