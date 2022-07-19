16dc8daeaa49b5f75c39a8df6c7d00484ee0e43b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-19 17:12:53 +0300

    Add readdir function

---
+++ L02_Node.jsStreamsAndUtilities/file-system/readdir.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readdir('./L02_Node.jsStreamsAndUtilities/file-system', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(data);
+});
