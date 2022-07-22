a1568c4bce7243b1d81790150f26b9ee12bb4582
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-22 18:42:05 +0300

    Edit handlebars file

--- L06_WorkshopMongoDBAndMongoose/src/config/handlebars.js
+++ L06_WorkshopMongoDBAndMongoose/src/config/handlebars.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     app.engine('hbs', handlebars.engine({
         extname: 'hbs'
     }));
-    
+
     app.set('view engine', 'hbs');
     app.set('views', './src/views');
 };
