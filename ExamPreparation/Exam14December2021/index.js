5300be01fa7339671ed4b4f0d0eb5cc50daf1f01
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-24 14:39:48 +0300

    Config express app

--- ExamPreparation/Exam14December2021/index.js
+++ ExamPreparation/Exam14December2021/index.js
@@ -3,5 +3,8 @@ const express = require('express');
 const { PORT } = require('./config/env');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
