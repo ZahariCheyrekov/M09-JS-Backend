6d7af6bc3adf687ea31cc4d36f12ad1bed0cefd4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-24 14:41:34 +0300

    Configure handlebars

--- ExamPreparation/Exam14December2021/index.js
+++ ExamPreparation/Exam14December2021/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const { PORT } = require('./config/env');
 
 const app = express();
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
