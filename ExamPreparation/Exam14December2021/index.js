4117aa6e1c10ee4cd8306da8419c409e2a13fd09
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-24 14:47:23 +0300

    Import router

--- ExamPreparation/Exam14December2021/index.js
+++ ExamPreparation/Exam14December2021/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 
 const { PORT } = require('./config/env');
+const routes = require('./router');
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
