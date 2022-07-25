4377bf5d857fbcc00de444a0db07aa1ee022c1b4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-25 15:57:00 +0300

    Edit auth middleware redirect

--- ExamPreparation/Exam14December2021/middlewares/authMiddleware.js
+++ ExamPreparation/Exam14December2021/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ exports.auth = (req, res, next) => {
         jwt.verify(token, SECRET, ((err, decodedToken) => {
             if (err) {
                 res.clearCookie(COOKIE_SESSION_NAME);
-                return next(err);
+                res.redirect('/login');
             }
 
             req.user = decodedToken;
