9c327e3a7d3344e59b55f07a762266f399687679
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-25 15:41:08 +0300

    Add route guards

--- ExamPreparation/Exam14December2021/controllers/authController.js
+++ ExamPreparation/Exam14December2021/controllers/authController.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
+
 const authService = require('../services/authService');
 const { COOKIE_SESSION_NAME } = require('../constants');
+const { isAuth, isGuest } = require('../middlewares/authMiddleware');
 
-router.get('/login', (req, res) => {
+router.get('/login', isGuest, (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest, async (req, res) => {
     const { username, password } = req.body;
     const user = await authService.login(username, password);
     const token = await authService.createToken(user);
@@ -15,11 +17,11 @@ router.post('/login', async (req, res) => {
     res.redirect('/');
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', isGuest, (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', isGuest, async (req, res) => {
     const { password, repeatPassword, ...userData } = req.body;
 
     if (password !== repeatPassword) {
@@ -37,7 +39,7 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', isAuth, (req, res) => {
     res.clearCookie(COOKIE_SESSION_NAME);
     res.redirect('/');
 });
