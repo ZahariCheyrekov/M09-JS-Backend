c05c9ed36db0f340cb9f20fcdd738f2ac64e2fb6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-25 13:59:27 +0300

    Add mongoose connection

--- ExamPreparation/Exam14December2021/config/initDB.js
+++ ExamPreparation/Exam14December2021/config/initDB.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose');
 const { DB_QUERYSTRING } = require('./env');
 
 mongoose.connect(DB_QUERYSTRING);
+
+mongoose.connection.on('open', () => console.log('Db is connected!'));
