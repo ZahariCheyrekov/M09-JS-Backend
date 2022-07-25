279305536957ba8dcb71514da388436455420b8b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-25 15:00:58 +0300

    Save cookie in login function

--- ExamPreparation/Exam14December2021/controllers/authController.js
+++ ExamPreparation/Exam14December2021/controllers/authController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authService = require('../services/authService');
+const { COOKIE_SESSION_NAME } = require('../constants');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
@@ -9,6 +10,9 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await authService.login(username, password);
     const token = await authService.createToken(user);
+
+    res.cookie(COOKIE_SESSION_NAME, token);
+    res.redirect('/');
 });
 
 router.get('/register', (req, res) => {
@@ -23,7 +27,10 @@ router.post('/register', async (req, res) => {
     }
 
     try {
-        await authService.create({ password, ...userData });
+        const createdUser = await authService.create({ password, ...userData });
+        const token = await authService.createToken(createdUser);
+
+        res.cookie(COOKIE_SESSION_NAME, token);
         res.redirect('/login');
     } catch (error) {
         return res.render('auth/register', { error: 'db error' });
