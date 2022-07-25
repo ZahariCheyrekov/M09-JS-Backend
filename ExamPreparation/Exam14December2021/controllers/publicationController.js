3cf6cb58593cf73e7e1291d6884a43ae14dbf455
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-25 19:26:02 +0300

    Edit publication controller gallery

--- ExamPreparation/Exam14December2021/controllers/publicationController.js
+++ ExamPreparation/Exam14December2021/controllers/publicationController.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const publicationService = require('../services/publicationService');
 
-router.use(isAuth);
+router.get('/', (req, res) => {
+    res.render('publication');
+});
 
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('publication/create');
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const publicationData = { ...req.body, author: req.user._id };
     await publicationService.create(publicationData);
 
