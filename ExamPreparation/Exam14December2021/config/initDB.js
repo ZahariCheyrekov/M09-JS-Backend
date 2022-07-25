733773a3715e5b3e88c946e5aa614fca769bc4ae
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-25 14:01:58 +0300

    Add db init function

--- ExamPreparation/Exam14December2021/config/initDB.js
+++ ExamPreparation/Exam14December2021/config/initDB.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const { DB_QUERYSTRING } = require('./env');
 
-mongoose.connect(DB_QUERYSTRING);
-
-mongoose.connection.on('open', () => console.log('Db is connected!'));
+exports.dbInit = () => {
+    mongoose.connection.on('open', () => console.log('Db is connected!'));
+    return mongoose.connect(DB_QUERYSTRING);
+};
