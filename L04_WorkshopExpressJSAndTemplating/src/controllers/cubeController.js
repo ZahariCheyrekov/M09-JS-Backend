24f302c18e30e245d78fb09f53b547dfd0b70893
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-31 22:11:24 +0300

    Edit cube controller

--- L04_WorkshopExpressJSAndTemplating/src/controllers/cubeController.js
+++ L04_WorkshopExpressJSAndTemplating/src/controllers/cubeController.js
@@ -9,12 +9,10 @@ router.get('/create', (req, res) => {
 router.post('/create', async (req, res) => {
     const cube = req.body;
 
-    // Validate
     if (cube.name.length < 2) {
         return res.status(400).send('Invalid request');
     }
 
-    // Save data
     try {
         await cubeService.save(cube);
 
@@ -26,7 +24,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/details/:id', (req, res) => {
     const cube = cubeService.getOne(req.params.id);
-    
+
     res.render('details', { cube });
 });
 
