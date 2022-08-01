f07e462d398aed24ee0ad9172390e1d650626189
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-01 19:18:49 +0300

    Edit file system demo js

--- L02_Node.jsStreamsAndUtilities/file-system/demo.js
+++ L02_Node.jsStreamsAndUtilities/file-system/demo.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
 // const text = fs.readFileSync('./L02_Node.jsStreamsAndUtilities/file-system/text.txt', { encoding: 'utf-8' });
-fs.readFile(
-    './L02_Node.jsStreamsAndUtilities/file-system/text.txt', { encoding: 'utf-8' },
+fs.readFile('./L02_Node.jsStreamsAndUtilities/file-system/text.txt', { encoding: 'utf-8' },
     (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
-
         console.log(data);
     });
