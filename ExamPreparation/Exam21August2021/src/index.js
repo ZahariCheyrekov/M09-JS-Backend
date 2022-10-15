c8294c1cae3132b5a5e663d578665d594afaab11
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-15 17:42:37 +0300

    Configure constants and index hbs file

--- ExamPreparation/Exam21August2021/src/index.js
+++ ExamPreparation/Exam21August2021/src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
+const { PORT } = require('./constants');
+
 const app = express();
 
 require('./config/expressConfig.js')(app);
 require('./config/hbsConfig.js')(app);
 
-const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port http://localhost:${PORT}/`));
