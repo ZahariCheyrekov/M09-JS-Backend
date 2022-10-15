2a70b47bdb40ecbb439c1d486c1db45f43959ba0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-15 17:57:42 +0300

    Implement routes and add home controller

--- ExamPreparation/Exam21August2021/src/index.js
+++ ExamPreparation/Exam21August2021/src/index.js
@@ -4,12 +4,11 @@ const { PORT } = require('./constants');
 
 const app = express();
 
+const routes = require('./routes.js');
+
 require('./config/expressConfig.js')(app);
 require('./config/hbsConfig.js')(app);
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port http://localhost:${PORT}/`));
