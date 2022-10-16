d9596fd369af165a51c1270266a39a749e592b12
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-16 12:25:42 +0300

    Add housing details route

--- ExamPreparation/Exam21August2021/src/controllers/housingController.js
+++ ExamPreparation/Exam21August2021/src/controllers/housingController.js
@@ -3,13 +3,6 @@ const router = require('express').Router();
 const housingService = require('../services/housingService.js');
 const { isAuth } = require('../middlewares/authMiddleware.js');
 
-router.get('/:housingId/details', async (req, res) => {
-    const housing = await housingService.getOne(req.params.housingId);
-    const isOnwer = housing.owner === req.user?._id;
-
-    res.render('housing/details', { ...housing, isOnwer });
-});
-
 router.get('/', async (req, res) => {
     const housings = await housingService.getAll();
 
@@ -25,5 +18,19 @@ router.post('/create', isAuth, async (req, res) => {
 
     res.redirect('/housing');
 });
+
+router.get('/:housingId/details', async (req, res) => {
+    const housing = await housingService.getOne(req.params.housingId);
+    const isOnwer = String(housing.owner) === req.user?._id;
+
+    const tenants = housing.getTenants();
+    const housingData = await housing.toObject();
+    const isAvailable = housing.availablePieces > 0;
+    const isRented = housing.tenants.some(tenant => tenant._id === req.user._id);
+
+    res.render('housing/details', { ...housingData, isOnwer, tenants, isAvailable, isRented });
+});
+
+
 
 module.exports = router;
