08009c78f697cbbace15ba1b276ce5911bc0cb34
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-16 11:22:26 +0300

    Implement get top houses service

--- ExamPreparation/Exam21August2021/src/services/housingService.js
+++ ExamPreparation/Exam21August2021/src/services/housingService.js
@@ -3,3 +3,7 @@ const Housing = require("../models/Housing");
 exports.create = async (housingData) => {
     return await Housing.create(housingData);
 }
+
+exports.getTopHouses = async () => {
+    return await Housing.find().sort({ createdAt: -1 }).limit(3).lean();
+}
