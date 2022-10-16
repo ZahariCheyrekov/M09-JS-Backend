9ccd01af5b95b92dba431266acfc335a6304d3fb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-16 11:22:35 +0300

    Fix housing controller

--- ExamPreparation/Exam21August2021/src/controllers/housingController.js
+++ ExamPreparation/Exam21August2021/src/controllers/housingController.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const housingService = require('../services/housingService.js');
 
-router.get('/', (req, res) => {
-    res.render('housing');
+router.get('/', async (req, res) => {
+    const housings = await housingService.getTopHouses();
+    
+    res.render('housing', { housings });
 });
 
 router.get('/create', (req, res) => {
