e626da69aac749f7cb99d8647fc6b4daf0cfd00b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-16 10:51:15 +0300

    Create housing mongoose schema

---
+++ ExamPreparation/Exam21August2021/src/models/Housing.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const housingSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['Apartment', 'Villa', 'House'],
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    availablePieces: {
+        type: Number,
+        required: true
+    },
+
+});
+
+const Housing = mongoose.model('User', housingSchema);
+module.exports = Housing;
