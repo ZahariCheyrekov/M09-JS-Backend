41fca0610500d15c244ff0f071102d496176936c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-17 12:42:28 +0300

    Configure server file

--- ExamPreparation/Exam16December2020/src/index.js
+++ ExamPreparation/Exam16December2020/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
+const expressConfig = require('./config/express.js');
+
+const { configDatabase } = require('./config/database.js');
+
 const app = express();
+expressConfig(app);
 
 app.get('/', (req, res) => res.send('It works!'));
 
-app.listen(5001);
+configDatabase(app);
