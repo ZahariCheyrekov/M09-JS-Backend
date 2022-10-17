16c5f3d859612cf31189c2d3ae05ef25e0e20657
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-17 18:21:39 +0300

    Implement register route

--- ExamPreparation/Exam16December2020/src/controllers/authController.js
+++ ExamPreparation/Exam16December2020/src/controllers/authController.js
@@ -1,11 +1,51 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+
 
 router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.post(
+    '/register',
+    body('username').isLength({ min: 3 }),
+    body('rePass').custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error('Passwords don\'t match!');
+        }
+        return true;
+    }),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+
+        try {
+            if (errors.length > 0) {
+                throw new Error('Validation error!');
+            }
+
+            await req.auth.register(req.body.username, req.body.password);
+
+            res.redirect('/auth/register');
+        } catch (error) {
+            const ctx = {
+                errors,
+                userData: {
+                    username: req.body.username
+                }
+            }
+            res.render('register', ctx);
+        }
+    }
+);
+
+
 router.get('/login', (req, res) => {
     res.render('login');
 });
+
+router.post('/login', (req, res) => {
+    res.render('login');
+});
+
 
 module.exports = router;
