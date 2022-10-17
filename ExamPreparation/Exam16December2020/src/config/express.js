764deedce6c8aca689cc1a3b4cc511257155cf92
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-17 18:04:09 +0300

    Configure handlebars engine

--- ExamPreparation/Exam16December2020/src/config/express.js
+++ ExamPreparation/Exam16December2020/src/config/express.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
+    app.engine('hbs', engine({
+        extname: 'hbs'
+    }));
+    app.set('views', path.resolve(__dirname, '../views'));
+    app.set('view engine', 'hbs');
+
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
