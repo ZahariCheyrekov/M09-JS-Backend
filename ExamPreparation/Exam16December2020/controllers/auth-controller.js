34994404c7e0a6650e308ce51112779180339eb9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 13:29:30 +0300

    Implement auth controller

---
+++ ExamPreparation/Exam16December2020/controllers/auth-controller.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.post(
+    '/register',
+    body('username').isLength({ min: 3 }),
+    body('rePass').custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error('Passwords don\'t match!');
+        }
+        return true;
+    }),
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        try {
+            if (errors.length > 0) {
+                throw new Error('Validation error');
+            }
+            await req.auth.register(req.body.username, req.body.password);
+
+            res.redirect('/');
+        } catch (error) {
+            const ctx = {
+                errors,
+                userData: {
+                    username: req.body.username
+                }
+            }
+            res.render('register', ctx);
+        }
+    }
+);
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', (req, res) => {
+});
+
+module.exports = router;
