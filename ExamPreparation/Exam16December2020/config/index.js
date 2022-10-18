b0cd8906e460cf46c0acc82b80a6ce8ccff4ffbd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 12:21:48 +0300

    Add token secret and cookie name

--- ExamPreparation/Exam16December2020/config/index.js
+++ ExamPreparation/Exam16December2020/config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: 5001,
-    CONNECTION_URL: 'mongodb://localhost:27017/book-uni'
+    CONNECTION_URL: 'mongodb://localhost:27017/book-uni',
+    TOKEN_SECRET: 'THIS IS SECURE',
+    COOKIE_NAME: 'SESSION_TOKEN'
 }
