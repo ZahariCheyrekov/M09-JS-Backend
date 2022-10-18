35c17495b60c66b555c99512f4a2e63b635b7bc8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 12:09:00 +0300

    Create auth middleware functions

---
+++ ExamPreparation/Exam16December2020/middlewares/auth.js
@@ -0,0 +1,14 @@
+function init() {
+    return function (req, res, next) {
+
+        next();
+    }
+}
+
+async function register(username, password) {
+
+}
+
+async function login(username, password) {
+
+}
