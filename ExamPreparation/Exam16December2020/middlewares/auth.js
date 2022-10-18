63cc326ad74c38ad40a8c45169018d002f6e2e03
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 12:12:44 +0300

    Implement register function

--- ExamPreparation/Exam16December2020/middlewares/auth.js
+++ ExamPreparation/Exam16December2020/middlewares/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcrypt');
+
+const userService = require('../services/user-service');
+
 function init() {
     return function (req, res, next) {
 
@@ -6,7 +10,14 @@ function init() {
 }
 
 async function register(username, password) {
+    const existingUser = await userService.getUserByUsername(username);
 
+    if (existingUser) {
+        throw new Error('Username is already taken!');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userService.createUser(username, hashedPassword);
 }
 
 async function login(username, password) {
