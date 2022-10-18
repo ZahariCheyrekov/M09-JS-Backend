566348bab29cc3a0d982ef6cf91de7f8a186e74d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 13:34:19 +0300

    Configure auth middleware

--- ExamPreparation/Exam16December2020/config/express-config.js
+++ ExamPreparation/Exam16December2020/config/express-config.js
@@ -1,7 +1,10 @@
-const hbs = require('express-handlebars');
 const path = require('path');
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
+
+const authMiddleware = require('../middlewares/auth');
 
 module.exports = (app) => {
     app.engine('hbs', hbs.engine({
@@ -13,4 +16,5 @@ module.exports = (app) => {
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+    app.use(authMiddleware());
 }
