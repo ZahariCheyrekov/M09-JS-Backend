712e98c616add6c597cddd82ef7c0f03d03a0d06
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 12:21:36 +0300

    Fix login and implement generate token function

--- ExamPreparation/Exam16December2020/middlewares/auth.js
+++ ExamPreparation/Exam16December2020/middlewares/auth.js
@@ -1,10 +1,23 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const { TOKEN_SECRET, COOKIE_NAME } = require('../config');
 const userService = require('../services/user-service');
 
 function init() {
     return function (req, res, next) {
+        req.auth = {
+            async register(username, password) {
+                const token = await register(username, password);
+                res.cookies(COOKIE_NAME, token);
+            },
+            async login() {
 
+            },
+            logout() {
+
+            }
+        }
         next();
     }
 }
@@ -18,8 +31,29 @@ async function register(username, password) {
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await userService.createUser(username, hashedPassword);
+
+    return generateToken(user);
 }
 
 async function login(username, password) {
+    const user = await userService.getUserByUsername(username);
 
+    if (!user) {
+        throw new Error('No such user!');
+    }
+
+    const hasMatch = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!hasMatch) {
+        throw new Error('Invalid password!');
+    }
+
+    return generateToken(user);
+}
+
+function generateToken(userData) {
+    return jwt.sign({
+        _id: userData._id,
+        username: userData.username
+    }, TOKEN_SECRET);
 }
