18da40803be76fdbfd6198a9212bb29cddab6751
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-18 11:57:31 +0300

    Configure database

--- ExamPreparation/Exam16December2020/index.js
+++ ExamPreparation/Exam16December2020/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 
+const { PORT } = require('./config');
+const { configDatabase } = require('./config/database-config');
+
+
 const app = express();
 
+
+configDatabase(app, PORT);
