505cd0a16bced9e7d063eb1201f85819c35c1123
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 19:15:08 +0300

    Create auth controller post route

--- ExamPreparation/Exam21August2021/src/controllers/auth-controller.js
+++ ExamPreparation/Exam21August2021/src/controllers/auth-controller.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
+const authService = require('../services/auth-service');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+    
 });
 
 module.exports = router;
