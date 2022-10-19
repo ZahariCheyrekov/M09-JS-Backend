d9fcc94c0ba3736697860b3748fddb6af109e12c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 21:35:11 +0300

    Implement login service

--- ExamPreparation/Exam21August2021/src/services/auth-service.js
+++ ExamPreparation/Exam21August2021/src/services/auth-service.js
@@ -1,6 +1,35 @@
-const { User } = require('../models/User');
+const jwt = require('jsonwebtoken');
 
-exports.login = () => {
+const { User } = require('../models/User');
+const { JWT_SECRET } = require('../constants');
+
+exports.login = async ({ username, password }) => {
+
+    try {
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            throw new Error('Invalid username or password');
+        }
+
+        const isValid = await user.validatePassword(password);
+
+        if (!isValid) {
+            throw new Error('Invalid username or password');
+        }
+
+        const payload = {
+            _id: user.id,
+            name: user.name,
+            username: user.username
+        }
+
+        const token = jwt.sign(payload, JWT_SECRET);
+        return token;
+
+    } catch (error) {
+
+    }
 
 }
 
