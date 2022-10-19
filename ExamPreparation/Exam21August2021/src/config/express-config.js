e4a28c62e534095666ffb3c3adb7fdb6600abfa3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 19:14:53 +0300

    Fix express configuration

--- ExamPreparation/Exam21August2021/src/config/express-config.js
+++ ExamPreparation/Exam21August2021/src/config/express-config.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const expressConfig = (app) => {
     app.use('/static', express.static(path.resolve(__dirname, '../public')));
+    app.use(express.urlencoded({ extended: true }));
 }
 
 module.exports = expressConfig;
