d1da07208fbde2ac2970f33e700cdc114d02255e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 20:47:09 +0300

    Add cookie parser to express

--- ExamPreparation/Exam21August2021/src/config/express-config.js
+++ ExamPreparation/Exam21August2021/src/config/express-config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const expressConfig = (app) => {
     app.use('/static', express.static(path.resolve(__dirname, '../public')));
     app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
 }
 
 module.exports = expressConfig;
