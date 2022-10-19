eb6f9f509111c012c4fead72cb38cb15ab689c9c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 18:28:54 +0300

    Configure express

--- ExamPreparation/Exam21August2021/src/index.js
+++ ExamPreparation/Exam21August2021/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
+
 require('./config/hbs-config')(app);
+require('./config/express-config')(app);
 
 app.get('/', (req, res) => {
     res.send('Application is running correctly!');
