a34f53a3e10cb5c05d0aaa3ef4eb22fa8729eda6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 18:28:45 +0300

    Configure express

--- ExamPreparation/Exam21August2021/src/config/express-config.js
+++ ExamPreparation/Exam21August2021/src/config/express-config.js
@@ -1,2 +1,7 @@
 const express = require('express');
 
+const expressConfig = (app) => {
+    app.use('/static', express.static(path.resolve(__dirname, '../public')));
+}
+
+module.exports = expressConfig;
