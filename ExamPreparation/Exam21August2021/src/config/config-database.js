6a38ea885a5742f9c9ea1acf6e9ca5012ee7e747
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 19:02:30 +0300

    Add database connection url

--- ExamPreparation/Exam21August2021/src/config/config-database.js
+++ ExamPreparation/Exam21August2021/src/config/config-database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+const { CONNECTION_URL } = require('../constants');
+
 const configDatabase = (app, PORT) => {
-    mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => app.listen(PORT, () => console.log(`Server is listening on: http://localhost:${PORT}`)))
         .catch((error) => console.log(error));
 }
