9915c1b597e34b6fea113d51a337dc3284e24b8a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-19 18:29:00 +0300

    Fix hbs engine

--- ExamPreparation/Exam21August2021/src/config/hbs-config.js
+++ ExamPreparation/Exam21August2021/src/config/hbs-config.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const hbs = require('express-handlebars');
 
 const hbsConfig = (app) => {
     app.engine('hbs', hbs.engine({
         extname: 'hbs'
     }));
+    app.set('views'.path.resoleve(__dirname, '../views'));
     app.set('view engine', 'hbs');
 }
 
