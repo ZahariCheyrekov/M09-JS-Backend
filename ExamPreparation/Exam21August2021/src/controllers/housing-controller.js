1a3713d5bbc47c3141ce984ef961282d5563275b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-20 11:01:12 +0300

    Implement create housing route

--- ExamPreparation/Exam21August2021/src/controllers/housing-controller.js
+++ ExamPreparation/Exam21August2021/src/controllers/housing-controller.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 
+const housingService = require('../services/housing-service');
+
 router.get('/', (req, res) => {
     res.render('housing');
 });
 
 router.get('/create', (req, res) => {
     res.render('housing/create');
+});
+
+router.post('/create', async (req, res) => {
+    console.log(req.body);
+    const housing = await housingService.create(req.body);
+
+    res.redirect('/');
 });
 
 module.exports = router;
