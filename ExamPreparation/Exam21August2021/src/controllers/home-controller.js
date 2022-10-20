c2fab28fb15a7e04635f1088ad6eea3510060f10
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-20 16:07:58 +0300

    Implement search functionality

--- ExamPreparation/Exam21August2021/src/controllers/home-controller.js
+++ ExamPreparation/Exam21August2021/src/controllers/home-controller.js
@@ -8,4 +8,11 @@ router.get('/', async (req, res) => {
     res.render('home', { title: 'Home Page', housings });
 });
 
+router.get('/search', async (req, res) => {
+    const query = req.query.text;
+
+    const housings = await housingService.search(query);
+    res.render('housing/search', { housings });
+});
+
 module.exports = router;
