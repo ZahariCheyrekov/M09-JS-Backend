2bd4c1448dcb92ffe9bb7d8f6c4de887be193623
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-20 22:27:43 +0300

    Import profile controller and use it

--- ExamPreparation/Exam18August2022/src/routes.js
+++ ExamPreparation/Exam18August2022/src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/home-controller');
 const authController = require('./controllers/auth-controller');
 const bookController = require('./controllers/book-controller');
+const profileController = require('./controllers/profile-controller');
 
 router.use('/', homeController);
 router.use('/auth', authController);
 router.use('/books', bookController);
+router.use('/profile', profileController);
 router.use('*', (req, res) => res.render('404'));
 
 module.exports = router;
