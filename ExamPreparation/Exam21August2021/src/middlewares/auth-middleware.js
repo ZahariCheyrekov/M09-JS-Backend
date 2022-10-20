a008216181ff8dc83f0f7b9cb4e627ed11543447
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-20 10:21:21 +0300

    Add token to response locals

--- ExamPreparation/Exam21August2021/src/middlewares/auth-middleware.js
+++ ExamPreparation/Exam21August2021/src/middlewares/auth-middleware.js
@@ -9,6 +9,7 @@ exports.auth = (req, res, next) => {
         jwt.verify(token, JWT_SECRET)
             .then(decodedToken => {
                 req.user = decodedToken;
+                res.locals.user = decodedToken;
                 next();
             })
             .catch(err => {
