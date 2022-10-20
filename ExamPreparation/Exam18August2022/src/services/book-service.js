77fa34a7632752a49cd27c65d94205f9f2a23eb0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-20 21:43:02 +0300

    Implement get book by id

--- ExamPreparation/Exam18August2022/src/services/book-service.js
+++ ExamPreparation/Exam18August2022/src/services/book-service.js
@@ -1,5 +1,13 @@
 const Book = require('../models/Book');
 
+exports.getBookById = async (bookId) => {
+    try {
+        return await Book.findById(bookId);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 exports.getAllBooks = async () => {
     try {
         return await Book.find().lean();
@@ -11,6 +19,20 @@ exports.getAllBooks = async () => {
 exports.createBook = async (bookData) => {
     try {
         await Book.create(bookData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.wishBook = async (bookId, userId) => {
+    try {
+        await Book.findOneAndUpdate(
+            { _id: bookId },
+            {
+                $push: { wishingList: userId },
+            },
+            { runValidators: true }
+        );
     } catch (error) {
         console.log(error);
     }
