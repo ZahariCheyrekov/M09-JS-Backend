2dc23cbd6d192408a46de1d3ca4cc0e6fbac7626
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-20 18:35:30 +0300

    Remove files

--- ExamPreparation/Exam16December2020/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: 5001,
-    CONNECTION_URL: 'mongodb://localhost:27017/book-uni',
-    TOKEN_SECRET: 'THIS IS SECURE',
-    COOKIE_NAME: 'SESSION_TOKEN'
-}
--- ExamPreparation/Exam16December2020/config/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const authController = require('../controllers/auth-controller');
-
-router.use('/auth', authController);
-
-module.exports = router;
