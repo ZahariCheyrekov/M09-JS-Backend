604ef92c1625676f85fd43d9493d49c2e477dbfd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 19:21:04 +0300

    Fix home controller

--- ExamPreparation/Exam26February2022/src/controllers/home-controller.js
+++ ExamPreparation/Exam26February2022/src/controllers/home-controller.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
+const adsService = require('../services/ads-service');
+
+router.get('/', async (req, res) => {
+    const ads = await adsService.getTopAds();
+
+    res.render('home', { ads });
 });
 
 module.exports = router;
