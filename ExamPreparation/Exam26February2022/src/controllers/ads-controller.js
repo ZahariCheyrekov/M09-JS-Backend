fc9b35095fbfe4ddb2f333387e6775aecd89aced
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 19:45:41 +0300

    Fix create add controller

--- ExamPreparation/Exam26February2022/src/controllers/ads-controller.js
+++ ExamPreparation/Exam26February2022/src/controllers/ads-controller.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const adsService = require('../services/ads-service');
+const authService = require('../services/auth-service');
 
 router.get('/', async (req, res) => {
     const ads = await adsService.getAll();
@@ -14,13 +15,20 @@ router.get('/create', (req, res) => {
 
 router.post('/create', async (req, res) => {
     try {
-        await adsService.create(req.body);
+        await adsService.create(req.user._id, { ...req.body, author: req.user._id });
         res.redirect('/ads');
 
     } catch (error) {
         const err = Object.values(error.errors)[0].properties.message;
         res.render('crypto/create', { error: err });
     }
+});
+
+router.get('/:adId/details', async (req, res) => {
+    const ad = await adsService.getAd(req.params.adId);
+    const { email } = await authService.getUser(ad.author);
+
+    res.render('ads/details', { ...ad.toObject(), email });
 });
 
 module.exports = router;
