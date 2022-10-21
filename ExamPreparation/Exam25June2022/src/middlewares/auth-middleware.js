f50968dbc916c474930112bb8f2b07e928ec0a01
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 11:37:11 +0300

    Add is auth and is guest middlewares

--- ExamPreparation/Exam25June2022/src/middlewares/auth-middleware.js
+++ ExamPreparation/Exam25June2022/src/middlewares/auth-middleware.js
@@ -20,3 +20,19 @@ exports.auth = (req, res, next) => {
         next();
     }
 }
+
+exports.isAuth = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/auth/login');
+    }
+}
+
+exports.isGuest = (req, res, next) => {
+    if (req.user) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+}
