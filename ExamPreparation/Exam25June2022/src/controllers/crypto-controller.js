9326e747bf1485a97fc700a95baa0e3f6b20f318
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 10:14:00 +0300

    Add crypto controller routes

--- ExamPreparation/Exam25June2022/src/controllers/crypto-controller.js
+++ ExamPreparation/Exam25June2022/src/controllers/crypto-controller.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
 
+const cryptoService = require('../services/crypto-service');
+
+router.get('/', async (req, res) => {
+    const coins = await cryptoService.getAllCoins();
+
+    res.render('crypto', { coins });
+});
+
+router.get('/create', (req, res) => {
+    res.render('crypto/create');
+});
+
+router.post('/create', async (req, res) => {
+    await cryptoService.create(req.body);
+
+    res.redirect('/crypto');
+});
+
 module.exports = router;
