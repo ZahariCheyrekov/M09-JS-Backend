33a20006fa8c3376cb5915292946b7a2d2f98344
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 13:00:47 +0300

    Create auction mongoose schema and add validation

---
+++ ExamPreparation/Exam19April2022/src/models/Auction.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const auctionSchema = mongoose.Schema({
+    title: {
+        type: String,
+        min: [4, 'Title should be at least 4 charactes long'],
+        required: true
+    },
+    description: {
+        type: String,
+        max: [200, 'Description should be maximum 200 charactes long']
+    },
+    category: {
+        type: String,
+        enum: ['Vehicles', 'Real Estate', 'Electronics', 'Furniture', 'Other'],
+        required: true
+    },
+    imageUrl: {
+        type: String
+    },
+    price: {
+        type: Number,
+        validate: (priceAmount) => {
+            if (Number(priceAmount) <= 0) {
+                throw new Error('Price cannot be negative');
+            }
+        },
+        required: true
+    },
+    author: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    bidder: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
+});
+
+module.exports = mongoose.model('Auction', auctionSchema);
