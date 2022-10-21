30e5e6652ac8e6bc06e5da4b7ff9d583ddf57330
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 09:45:36 +0300

    Implement crypto schema

---
+++ ExamPreparation/Exam25June2022/src/models/Crypto.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const cryptoSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    buyCrypto: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Crypto', cryptoSchema);
