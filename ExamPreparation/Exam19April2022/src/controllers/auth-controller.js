ffdd4e6bd54ad11beea8a1cb0dd03ba4f80b489c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 13:06:56 +0300

    Implement auth controllers

--- ExamPreparation/Exam19April2022/src/controllers/auth-controller.js
+++ ExamPreparation/Exam19April2022/src/controllers/auth-controller.js
@@ -1,23 +1,54 @@
 const router = require('express').Router();
 
+const { COOKIE_NAME } = require('../constants');
+const authService = require('../services/auth-service');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
-    res.render('auth/login');
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login({ email, password });
+
+        res.cookie(COOKIE_NAME, token);
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
-    res.render('auth/register');
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (req.password !== req.rePassword) {
+        res.locals.errors = 'Password mismatch!';
+        res.render('auth/register');
+    }
+
+    try {
+        await authService.register(req.body);
+
+        const token = await authService.login({ email, password });
+
+        res.cookie(COOKIE_NAME, token);
+        res.redirect('/');
+
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/logout', (req, res) => {
-
-})
+    res.clearCookie(COOKIE_NAME);
+    res.redirect('/');
+});
 
 module.exports = router;
