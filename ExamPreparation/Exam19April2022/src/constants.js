789c0127b0d943bdb6a14e8f89a9696acf1cf863
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 12:19:17 +0300

    Add server constants

--- ExamPreparation/Exam19April2022/src/constants.js
+++ ExamPreparation/Exam19April2022/src/constants.js
@@ -1,5 +1,5 @@
 exports.PORT = 3000;
-exports.CONNECTION_URL = 'mongodb://localhost:27017/crypto';
+exports.CONNECTION_URL = 'mongodb://localhost:27017/auction-house';
 exports.SALT_ROUNDS = 10;
 exports.JWT_SECRET = 'THIS IS SECRET! >:)';
 exports.COOKIE_NAME = 'AUTH_COOKIE';
