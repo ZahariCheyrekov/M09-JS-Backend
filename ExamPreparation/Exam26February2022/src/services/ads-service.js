afb753d370bf44d504cfc4be1dac89c7a6c717e5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 19:21:22 +0300

    Create get top ads service

--- ExamPreparation/Exam26February2022/src/services/ads-service.js
+++ ExamPreparation/Exam26February2022/src/services/ads-service.js
@@ -4,6 +4,10 @@ exports.getAll = async () => {
     return await Ad.find().lean();
 }
 
+exports.getTopAds = async () => {
+    return await Ad.find().sort({ createdAt: -1 }).limit(3).lean();
+}
+
 exports.create = async (adData) => {
     return await Ad.create(adData);
 }
