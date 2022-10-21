50f4bf9f671768fcc26d6830663b19256b995d35
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 09:38:29 +0300

    Create user schema and its methods

---
+++ ExamPreparation/Exam25June2022/src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { SALT_ROUNDS } = require('../constants');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save', function (next) {
+    return bcrypt.hash(this.password, SALT_ROUNDS)
+        .then((hash) => {
+            this.password = hash;
+            next();
+        });
+});
+
+userSchema.method('validatePassword', async function (pass) {
+    return await bcrypt.compare(pass, this.password);
+});
+
+module.exports = mongoose.model('User', userSchema);
