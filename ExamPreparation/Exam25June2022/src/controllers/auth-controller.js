fc07603c312a6b3ccbaa268cf6172087c7b1b114
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 09:25:03 +0300

    Create auth controller

--- ExamPreparation/Exam25June2022/src/controllers/auth-controller.js
+++ ExamPreparation/Exam25June2022/src/controllers/auth-controller.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 
+const { COOKIE_NAME } = require('../constants');
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie(COOKIE_NAME);
+    res.redirect('/');
+});
+
 module.exports = router;
