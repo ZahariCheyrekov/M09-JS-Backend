99addfe4d2846da3b59ec1fbad2bb27f3696e7cf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 13:36:57 +0300

    Import auction controller and use it

--- ExamPreparation/Exam19April2022/src/routes.js
+++ ExamPreparation/Exam19April2022/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/home-controller');
 const authController = require('./controllers/auth-controller');
+const auctionController = require('./controllers/auction-controller');
 
 router.use('/', homeController);
 router.use('/auth', authController);
+router.use('/auction', auctionController);
 router.use('*', (req, res) => res.render('404'));
 
 module.exports = router;
