85fc5fabce4a1e8cf1a99e5f0308f2339c7e37bd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 13:06:48 +0300

    Add login and register service

---
+++ ExamPreparation/Exam19April2022/src/services/auth-service.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../constants');
+const User = require('../models/User');
+
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('User doesn\'t exist');
+    }
+
+    const isValid = await user.validatePassword(password);
+
+    if (!isValid) {
+        throw new Error('Invalid user password');
+    }
+
+    const payload = {
+        _id: user.id,
+        email: user.email
+    }
+
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+}
+
+exports.register = async (userData) => {
+    await User.create(userData);
+}
