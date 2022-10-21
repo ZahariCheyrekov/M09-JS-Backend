08f1bbe7c48470d3c61d4bd2993cd5c506b225ae
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 13:50:22 +0300

    Implement create auction routes

--- ExamPreparation/Exam19April2022/src/controllers/auction-controller.js
+++ ExamPreparation/Exam19April2022/src/controllers/auction-controller.js
@@ -8,4 +8,19 @@ router.get('/', async (req, res) => {
     res.render('auction', { auctions });
 });
 
+router.get('/create', (req, res) => {
+    res.render('auction/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await auctionService.create({ ...req.body, author: req.user._id });
+        res.redirect('/auction');
+
+    } catch (error) {
+        const err = Object.values(error.errors)[0].properties.message;
+        res.render('auction/create', { error: err });
+    }
+});
+
 module.exports = router;
