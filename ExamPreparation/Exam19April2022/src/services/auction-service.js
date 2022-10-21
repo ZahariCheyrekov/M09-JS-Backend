bdef811a5aa2bd23741202e57cc186553f5b611a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 13:50:39 +0300

    Implement create auction service

--- ExamPreparation/Exam19April2022/src/services/auction-service.js
+++ ExamPreparation/Exam19April2022/src/services/auction-service.js
@@ -3,3 +3,7 @@ const Auction = require('../models/Auction');
 exports.getAll = async () => {
     return await Auction.find().lean();
 }
+
+exports.create = async (auctionData) => {
+    return await Auction.create(auctionData);
+}
