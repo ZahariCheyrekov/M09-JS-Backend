33b95a59c90cf67ff969637091450f4c01ff4b52
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-21 18:26:46 +0300

    Implement user schema

---
+++ ExamPreparation/Exam26February2022/src/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { SALT_ROUNDS } = require('../constants');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    myAds: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Ad'
+        }
+    ]
+});
+
+userSchema.pre('save', function (next) {
+    return bcrypt.hash(this.password, SALT_ROUNDS)
+        .then((hash) => {
+            this.password = hash;
+            next();
+        });
+});
+
+userSchema.method('validatePassword', async function (pass) {
+    return await bcrypt.compare(pass, this.password);
+});
+
+module.exports = mongoose.model('User', userSchema);
