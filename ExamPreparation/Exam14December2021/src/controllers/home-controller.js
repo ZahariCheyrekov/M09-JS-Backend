044a012069095fd22bc3679426cd267ed0192de1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 17:11:58 +0300

    Implement home controller

--- ExamPreparation/Exam14December2021/src/controllers/home-controller.js
+++ ExamPreparation/Exam14December2021/src/controllers/home-controller.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const galleryService = require('../services/gallery-service');
+
 router.get('/', async (req, res) => {
-    res.render('home');
+    const publications = await galleryService.getAll();
+
+    res.render('home', { publications });
 });
 
 module.exports = router;
