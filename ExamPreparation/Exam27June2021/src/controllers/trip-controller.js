f7e136852a9fd97bda20453528e7db15b832b04e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 10:40:21 +0300

    Add trip details route

--- ExamPreparation/Exam27June2021/src/controllers/trip-controller.js
+++ ExamPreparation/Exam27June2021/src/controllers/trip-controller.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const tripService = require('../services/trip-service');
+const userService = require('../services/auth-service');
 
 router.get('/', async (req, res) => {
     const trips = await tripService.getTrips();
@@ -21,6 +22,27 @@ router.post('/offer', async (req, res) => {
         const err = Object.values(error.errors)[0].properties.message;
         res.render('crypto/create', { error: err });
     }
+});
+
+router.get('/:tripId/details', async (req, res) => {
+    const trip = await tripService.getTrip(req.params.tripId);
+    const { email: creatorEmail } = await userService.getUser(trip.creator);
+
+    const isOwner = String(trip.creator) === req.user?._id;
+    const availableSeats = trip.seats > 0;
+    const joinedBuddies = trip.buddies.length > 0;
+    const buddies = trip.buddies.join(', ');
+    const joined = !isOwner && trip.buddies.some(buddy => String(buddy) === req.user?._id);
+    console.log(joined)
+
+    res.render('trip/details', {
+        ...trip.toObject(),
+        creatorEmail,
+        isOwner,
+        availableSeats,
+        joinedBuddies,
+        buddies
+    });
 });
 
 module.exports = router;
