0406f0dd760bd0d79c599146ebb83ee495a39b23
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 10:09:32 +0300

    Add create trip service

--- ExamPreparation/Exam27June2021/src/services/trip-service.js
+++ ExamPreparation/Exam27June2021/src/services/trip-service.js
@@ -1,5 +1,16 @@
 const Trip = require('../models/Trip');
+const User = require('../models/User');
 
 exports.getTrips = async () => {
     return await Trip.find().lean();
+}
+
+exports.createTrip = async (userId, tripData) => {
+    const trip = await Trip.create(tripData);
+
+    return await User.findByIdAndUpdate(
+        { _id: userId },
+        { $push: { tripsHistory: trip._id } },
+        { runValidators: true }
+    );
 }
