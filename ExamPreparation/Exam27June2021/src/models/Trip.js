e0676e141277d9c2b306e72d0eb62ba94575c9d6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 09:12:30 +0300

    Create trip schema

---
+++ ExamPreparation/Exam27June2021/src/models/Trip.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const tripSchema = mongoose.Schema({
+    startPoint: {
+        type: String,
+        required: true
+    },
+    endPoint: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    carImage: {
+        type: String,
+        required: true
+    },
+    carBrand: {
+        type: String,
+        required: true
+    },
+    seats: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    buddies: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+module.exports = mongoose.model('Trip', tripSchema);
