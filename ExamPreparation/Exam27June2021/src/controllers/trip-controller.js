d51627a31d6e6a7810d4886715bb541ae42924ba
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 10:09:21 +0300

    Implement trip controller routes

--- ExamPreparation/Exam27June2021/src/controllers/trip-controller.js
+++ ExamPreparation/Exam27June2021/src/controllers/trip-controller.js
@@ -8,4 +8,19 @@ router.get('/', async (req, res) => {
     res.render('trip', { trips });
 });
 
+router.get('/offer', async (req, res) => {
+    res.render('trip/offer');
+});
+
+router.post('/offer', async (req, res) => {
+    try {
+        await tripService.createTrip(req.user._id, { ...req.body, creator: req.user._id });
+        res.redirect('/trip');
+
+    } catch (error) {
+        const err = Object.values(error.errors)[0].properties.message;
+        res.render('crypto/create', { error: err });
+    }
+});
+
 module.exports = router;
