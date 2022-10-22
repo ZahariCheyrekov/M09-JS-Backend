26a258479f6d4e7ed8f5d9d22fd3a2793edb4d1c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 17:29:11 +0300

    Implement gallery controller routes

--- ExamPreparation/Exam14December2021/src/controllers/gallery-controller.js
+++ ExamPreparation/Exam14December2021/src/controllers/gallery-controller.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
 
+const galleryService = require('../services/gallery-service');
+
 router.get('/', async (req, res) => {
-    
+    const publications = await galleryService.getAll();
+
+    res.render('gallery', { publications });
+});
+
+router.get('/create', async (req, res) => {
+    res.render('gallery/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await galleryService.createPublication(req.user._id, { ...req.body, author: req.user._id })
+        res.redirect('/gallery');
+
+    } catch (error) {
+        const errors = [];
+
+        for (const { properties } of Object.values(error.errors)) {
+            errors.push({ 'error': properties.message });
+        }
+
+        res.render('gallery/create', { errors });
+    }
 });
 
 module.exports = router;
