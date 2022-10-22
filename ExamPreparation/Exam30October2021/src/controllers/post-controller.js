5b3c30357ab213bdffe077d19d3636a0b99d75cd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 20:50:38 +0300

    Implement create post route

--- ExamPreparation/Exam30October2021/src/controllers/post-controller.js
+++ ExamPreparation/Exam30October2021/src/controllers/post-controller.js
@@ -4,8 +4,28 @@ const postService = require('../services/post-service');
 
 router.get('/', async (req, res) => {
     const posts = await postService.getAll();
-    
+
     res.render('posts', { posts });
+});
+
+router.get('/create', async (req, res) => {
+    res.render('posts/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await postService.createPost(req.user._id, { ...req.body, author: req.user._id });
+        res.redirect('/posts');
+
+    } catch (error) {
+        const errors = [];
+
+        for (const { properties } of Object.values(error.errors)) {
+            errors.push({ 'error': properties.message });
+        }
+
+        res.render('trip/offer', { errors });
+    }
 });
 
 module.exports = router;
