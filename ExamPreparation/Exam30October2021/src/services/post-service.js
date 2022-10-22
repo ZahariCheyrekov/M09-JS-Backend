dad76089f4b377faab9e256a70a910f4dc4f264d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 20:50:51 +0300

    Implement create post service

--- ExamPreparation/Exam30October2021/src/services/post-service.js
+++ ExamPreparation/Exam30October2021/src/services/post-service.js
@@ -1,5 +1,18 @@
 const Post = require('../models/Post');
+const User = require('../models/User');
+
+const userService = require('../services/auth-service');
 
 exports.getAll = async () => {
     return await Post.find().lean();
+}
+
+exports.createPost = async (userId, postData) => {
+    const post = await Post.create(postData);
+
+    return await User.findByIdAndUpdate(
+        { _id: userId },
+        { $push: { myPosts: post._id } },
+        { runValidators: true }
+    );
 }
