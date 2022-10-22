48c77b85d73aa31477087aa17175d4816fd86d99
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 21:40:02 +0300

    Add mongoose schema validation

--- ExamPreparation/Exam30October2021/src/models/Post.js
+++ ExamPreparation/Exam30October2021/src/models/Post.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: {
         type: String,
+        min: [6, 'Title should be at least 6 characters long'],
         requried: true
     },
     keyword: {
         type: String,
+        min: [6, 'Keyword should be at least 6 characters long'],
         requried: true
     },
     location: {
         type: String,
+        min: [15, 'Location name should be maximum 15 characters long'],
         requried: true
     },
     date: {
@@ -19,10 +22,12 @@ const postSchema = mongoose.Schema({
     },
     image: {
         type: String,
+        validate: /^https:?\/\//i,
         required: true
     },
     description: {
         type: String,
+        min: [8, 'Description should be at least 8 characters long'],
         requried: true
     },
     author: {
--- ExamPreparation/Exam30October2021/src/models/User.js
+++ ExamPreparation/Exam30October2021/src/models/User.js
@@ -6,18 +6,27 @@ const { SALT_ROUNDS } = require('../constants');
 const userSchema = mongoose.Schema({
     firstName: {
         type: String,
+        min: [3, 'First name should be at least 3 characters long'],
         required: true
     },
     lastName: {
         type: String,
+        min: [5, 'Last name should be at least 5 characters long'],
         required: true
     },
     email: {
         type: String,
+        validate: (email) => {
+            const regex = /\b(?<name>[A-Za-z]+)\@(?<domain>[A-Za-z]+)\.(?<extension>[A-Za-z]+)\b/g
+            if (!email.match(regex)) {
+                throw new Error('The email should be in format: <name>@<domain>.<extension>. Example: "petar@softuni.bg"');
+            }
+        },
         required: true
     },
     password: {
         type: String,
+        min: [4, 'Password should be at least 4 characters long'],
         required: true
     },
     myPosts: [
