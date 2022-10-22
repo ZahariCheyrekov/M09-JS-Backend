09fd6c1c7cbb2cc7f2eaf60bab4cfb6fbf11ebf0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 21:26:16 +0300

    Implement get people emails service

--- ExamPreparation/Exam30October2021/src/services/post-service.js
+++ ExamPreparation/Exam30October2021/src/services/post-service.js
@@ -11,12 +11,47 @@ exports.getAll = async () => {
     return await Post.find().lean();
 }
 
+exports.getPeopleEmails = async (postId) => {
+    const emails = [];
+
+    const { votes } = await this.getOne(postId);
+
+    for (const vote of votes) {
+        const email = await userService.getUserEmail(vote);
+        emails.push(email);
+    }
+
+    return emails.join(', ');
+}
+
 exports.createPost = async (userId, postData) => {
     const post = await Post.create(postData);
 
     return await User.findByIdAndUpdate(
         { _id: userId },
         { $push: { myPosts: post._id } },
+        { runValidators: true }
+    );
+}
+
+exports.upvote = async (postId, userId) => {
+    return await Post.findByIdAndUpdate(
+        { _id: postId },
+        {
+            $push: { votes: userId },
+            $inc: { rating: 1 }
+        },
+        { runValidators: true }
+    );
+}
+
+exports.downvote = async (postId, userId) => {
+    return await Post.findByIdAndUpdate(
+        { _id: postId },
+        {
+            $push: { votes: userId },
+            $inc: { rating: -1 }
+        },
         { runValidators: true }
     );
 }
