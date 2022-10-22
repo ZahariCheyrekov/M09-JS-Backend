6695faf3e66357f9c427236d564556e658992752
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-22 17:29:27 +0300

    Implement create publication service

--- ExamPreparation/Exam14December2021/src/services/gallery-service.js
+++ ExamPreparation/Exam14December2021/src/services/gallery-service.js
@@ -1,5 +1,16 @@
 const Publication = require('../models/Publication');
+const User = require('../models/User');
 
 exports.getAll = async () => {
     return await Publication.find().lean();
+}
+
+exports.createPublication = async (userId, publicationData) => {
+    const publication = await Publication.create(publicationData);
+
+    return await User.findByIdAndUpdate(
+        { _id: userId },
+        { $push: { myPublications: publication._id } },
+        { runValidators: true }
+    )
 }
