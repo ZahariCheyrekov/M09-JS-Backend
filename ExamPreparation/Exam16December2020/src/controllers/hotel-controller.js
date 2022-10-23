a74d1897b4620bfe87ed832db1de1d002088e819
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-23 09:16:56 +0300

    Implement create hotel route

--- ExamPreparation/Exam16December2020/src/controllers/hotel-controller.js
+++ ExamPreparation/Exam16December2020/src/controllers/hotel-controller.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
+const hotelService = require('../services/hotel-service');
 
+router.get('/create', async (req, res) => {
+    res.render('hotels/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await hotelService.create(req.user._id, { ...req.body, owner: req.user.username });
+        res.redirect('/');
+
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
