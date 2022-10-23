62c6b3c0fa65021cf376b621e3895ad8171d2aef
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-23 09:17:42 +0300

    Implement create hotel service and get one

--- ExamPreparation/Exam16December2020/src/services/hotel-service.js
+++ ExamPreparation/Exam16December2020/src/services/hotel-service.js
@@ -1,5 +1,20 @@
 const Hotel = require('../models/Hotel');
+const User = require('../models/User');
+
+exports.getOne = async (hotelId) => {
+    return await Hotel.findById(hotelId);
+}
 
 exports.getTopHotels = async () => {
-    return await Hotel.find().sort({ freeRooms: 1 }).limit(3).lean();
+    return await Hotel.find().sort({ freeRooms: -1, createdAt: -1 }).limit(3).lean();
+}
+
+exports.create = async (userId, hotelData) => {
+    const hotel = await Hotel.create(hotelData);
+
+    return await User.findByIdAndUpdate(
+        { _id: userId },
+        { $push: { offeredHotels: hotel._id } },
+        { runValidators: true }
+    )
 }
