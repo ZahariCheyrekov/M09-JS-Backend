5a6e33cc074769319bf97b157f2ddc4412cf11d9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-23 16:23:01 +0300

    Add theater details route

--- ExamPreparation/Exam28June2020/src/controllers/theater-controller.js
+++ ExamPreparation/Exam28June2020/src/controllers/theater-controller.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const authService = require('../services/auth-service');
 const theaterService = require('../services/theater-service');
 
 router.get('/create', (req, res) => {
@@ -9,7 +10,7 @@ router.get('/create', (req, res) => {
 router.post('/create', async (req, res) => {
     try {
         const isTheaterPublic = req.body.isPublic === 'on' ? true : false;
-        req.body.isPublic=isTheaterPublic;
+        req.body.isPublic = isTheaterPublic;
 
         await theaterService.createTheater({ ...req.body, author: req.user._id });
         res.redirect('/');
@@ -23,6 +24,16 @@ router.post('/create', async (req, res) => {
 
         res.render('theater/create', { errors });
     }
+});
+
+router.get('/:theaterId/details', async (req, res) => {
+    const theater = await theaterService.getOne(req.params.theaterId);
+    const { username: author } = await authService.getUser(theater.author);
+
+    const isOwner = String(theater.author) === req.user?._id;
+    const isLiked = theater.usersLiked.some(user => String(user) === req.user?._id);
+
+    res.render('theater/details', { ...theater.toObject(), author, isOwner, isLiked });
 });
 
 module.exports = router;
