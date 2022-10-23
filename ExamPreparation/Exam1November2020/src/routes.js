e8f5de4c9123044a5b74c70eed2b1cd253136b05
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-23 12:19:27 +0300

    Import course controller and use it

--- ExamPreparation/Exam1November2020/src/routes.js
+++ ExamPreparation/Exam1November2020/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/home-controller');
 const authController = require('./controllers/auth-controller');
+const courseController = require('./controllers/course-controller');
 
 router.use('/', homeController);
 router.use('/auth', authController);
+router.use('/courses', courseController);
 router.use('*', (req, res) => res.render('404'));
 
 module.exports = router;
