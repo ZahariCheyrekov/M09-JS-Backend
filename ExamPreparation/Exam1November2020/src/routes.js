e4f138548475f91eb4572d926be06236e58413d7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-23 11:44:43 +0300

    Fix app routes

--- ExamPreparation/Exam1November2020/src/routes.js
+++ ExamPreparation/Exam1November2020/src/routes.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/home-controller');
 const authController = require('./controllers/auth-controller');
-const hotelController = require('./controllers/hotel-controller');
 
 router.use('/', homeController);
 router.use('/auth', authController);
-router.use('/hotels', hotelController);
 router.use('*', (req, res) => res.render('404'));
 
 module.exports = router;
