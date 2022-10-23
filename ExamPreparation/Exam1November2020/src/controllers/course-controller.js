4bbcd8cb2bfcf2f4f868fe443e919e430771490a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-23 12:36:25 +0300

    Add course details route

--- ExamPreparation/Exam1November2020/src/controllers/course-controller.js
+++ ExamPreparation/Exam1November2020/src/controllers/course-controller.js
@@ -8,7 +8,7 @@ router.get('/create', async (req, res) => {
 
 router.post('/create', async (req, res) => {
     try {
-        await courseService.createCourse(req.body)
+        await courseService.createCourse({ ...req.body, owner: req.user._id });
         res.redirect('/');
 
     } catch (error) {
@@ -20,6 +20,15 @@ router.post('/create', async (req, res) => {
 
         res.render('course/create', { errors });
     }
+});
+
+router.get('/:courseId/details', async (req, res) => {
+    const course = await courseService.getOne(req.params.courseId);
+
+    const isOwner = String(course.owner) === req.user?._id;
+    const isEnrolled = course.usersEnrolled.some(user => String(user) === req.user?._id);
+
+    res.render('course/details', { ...course.toObject(), isOwner, isEnrolled });
 });
 
 module.exports = router;
