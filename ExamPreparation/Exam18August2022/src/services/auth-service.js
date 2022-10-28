5bacd3eac92d06c0f4009c3a54fa1f5a9606b0b7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-28 20:02:03 +0300

    Create user auth services

---
+++ ExamPreparation/Exam18August2022/src/services/auth-service.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../constants');
+const User = require('../models/User');
+
+exports.getUser = async (userId) => {
+    return await User.findById(userId);
+}
+
+exports.login = async ({ username, password }) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Invalid email or password');
+    }
+
+    const isValid = await user.validatePassword(password);
+
+    if (!isValid) {
+        throw new Error('Invalid email or password');
+    }
+
+    const payload = {
+        _id: user.id,
+        username: user.username,
+        email: user.email
+    }
+
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+}
+
+exports.register = async (userData) => {
+    await User.create(userData);
+}
