afb2b44faf3acc5f9352883051b44a2a532548d1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-28 21:21:49 +0300

    Add book schema validation

--- ExamPreparation/Exam18August2022/src/models/Book.js
+++ ExamPreparation/Exam18August2022/src/models/Book.js
@@ -3,22 +3,27 @@ const mongoose = require('mongoose');
 const bookSchema = mongoose.Schema({
     title: {
         type: String,
+        min: [2, 'Username should be at least 2 characters long'],
         required: true
     },
     author: {
         type: String,
+        min: [5, 'Username should be at least 5 characters long'],
         required: true
     },
     image: {
         type: String,
+        validate: /^https?:\/\//i,
         required: true
     },
     bookReview: {
         type: String,
+        min: [10, 'Username should be at least 10 characters long'],
         required: true
     },
     genre: {
         type: String,
+        min: [3, 'Username should be at least 3 characters long'],
         required: true
     },
     stars: {
