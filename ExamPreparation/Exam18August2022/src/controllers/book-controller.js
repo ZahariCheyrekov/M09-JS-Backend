247c8e3db363cb33440ac3d06849fd31b1f13240
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-28 20:47:39 +0300

    Add create book routes

--- ExamPreparation/Exam18August2022/src/controllers/book-controller.js
+++ ExamPreparation/Exam18August2022/src/controllers/book-controller.js
@@ -4,8 +4,29 @@ const bookService = require('../services/book-service');
 
 router.get('/', async (req, res) => {
     const books = await bookService.getAll();
-    
+
     res.render('book', { books });
+});
+
+router.get('/create', (req, res) => {
+    res.render('book/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await bookService.createBook({ ...req.body, owner: req.user._id });
+        res.redirect('/books');
+
+    } catch (error) {
+        console.log(error);
+        const errors = [];
+
+        for (const { properties } of Object.values(error.errors)) {
+            errors.push({ 'error': properties.message });
+        }
+
+        res.render('theater/create', { errors });
+    }
 });
 
 module.exports = router;
