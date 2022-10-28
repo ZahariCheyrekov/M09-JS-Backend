46b5079f47c1b01acc6437324540b8df554a1974
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-28 20:47:30 +0300

    Implement create book service

--- ExamPreparation/Exam18August2022/src/services/book-service.js
+++ ExamPreparation/Exam18August2022/src/services/book-service.js
@@ -3,3 +3,7 @@ const Book = require('../models/Book');
 exports.getAll = async () => {
     return await Book.find().lean();
 }
+
+exports.createBook = async (bookData) => {
+    return await Book.create(bookData);
+}
