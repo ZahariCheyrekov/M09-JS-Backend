03f53026f065f731294f8e7b1ebb603c7f47d7d5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-29 18:59:39 +0300

    Create mongoose crypto schema

---
+++ ExamPreparation/Exam25June2022/src/models/Crypto.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const cryptoSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['crypto-wallet', 'credit-card', 'debit-card', 'paypal'],
+        required: true
+    },
+    buyCrypto: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Crypto', cryptoSchema);
