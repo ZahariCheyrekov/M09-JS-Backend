0f0eb5849c245576ffa122cbff1ecf6e55365a7e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-29 19:21:11 +0300

    Create crypto controller

---
+++ ExamPreparation/Exam25June2022/src/controllers/crypto-controller.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const cryptoService = require('../services/crypto-service');
+
+router.get('/create', (req, res) => {
+    res.render('crypto/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await cryptoService.createCrypto({ ...req.body, owner: req.user?._id });
+        res.redirect('/crypto');
+
+    } catch (error) {
+        const errors = [];
+
+        for (const { properties } of Object.values(error.errors)) {
+            errors.push({ 'error': properties.message });
+        }
+
+        res.render('crypto/create', { errors });
+    }
+});
+
+module.exports = router;
