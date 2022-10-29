013ec2f98482458ebb03a61c6e8e75bf0a90b180
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-29 18:45:41 +0300

    Remove files

--- ExamPreparation/Exam25June2022/src/constants.js
+++
@@ -1,5 +0,0 @@
-exports.PORT = 3000;
-exports.CONNECTION_URL = 'mongodb://localhost:27017/crypto';
-exports.SALT_ROUNDS = 10;
-exports.JWT_SECRET = 'THIS IS SECRET! >:)';
-exports.COOKIE_NAME = 'AUTH_COOKIE';
