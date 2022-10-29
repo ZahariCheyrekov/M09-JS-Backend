0ff208b28f908e349158535af870162de105a211
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-29 19:21:03 +0300

    Import crypto controller and use it

--- ExamPreparation/Exam25June2022/src/routes.js
+++ ExamPreparation/Exam25June2022/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/home-controller');
 const authController = require('./controllers/auth-controller');
+const cryptoController = require('./controllers/crypto-controller');
 
 router.use('/', homeController);
 router.use('/auth', authController);
+router.use('/crypto', cryptoController);
 router.use('*', (req, res) => res.render('404'));
 
 module.exports = router;
