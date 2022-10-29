618a3bcd4de80869d68026f252a088ec3ceded40
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-29 21:53:45 +0300

    Add crypto schema validation

--- ExamPreparation/Exam25June2022/src/models/Crypto.js
+++ ExamPreparation/Exam25June2022/src/models/Crypto.js
@@ -3,18 +3,26 @@ const mongoose = require('mongoose');
 const cryptoSchema = mongoose.Schema({
     name: {
         type: String,
+        min: [2, 'Name should be at least 2 characters long'],
         required: true
     },
     image: {
         type: String,
+        validate: /^https?:\/\//i,
         required: true
     },
     price: {
         type: Number,
+        validate: (priceValue) => {
+            if (priceValue <= 0) {
+                throw new Error('Price should be positive number!');
+            }
+        },
         required: true
     },
     description: {
         type: String,
+        min: [10, 'Name should be at least 10 characters long'],
         required: true
     },
     paymentMethod: {
