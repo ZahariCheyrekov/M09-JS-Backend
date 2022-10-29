c79783a6f2ba68a8d8ac91c1c50bb39ab21ed4d1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-29 21:13:52 +0300

    Add get all cryptoes service

--- ExamPreparation/Exam25June2022/src/services/crypto-service.js
+++ ExamPreparation/Exam25June2022/src/services/crypto-service.js
@@ -1,5 +1,9 @@
 const Crypto = require('../models/Crypto');
 
+exports.getAll = async () => {
+    return await Crypto.find().lean();
+}
+
 exports.createCrypto = async (cryptoData) => {
     return await Crypto.create(cryptoData);
 }
