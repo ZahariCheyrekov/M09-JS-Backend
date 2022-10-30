3864482df6bcdec242ed76d587f19f91ee9abe74
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-30 14:06:46 +0200

    Add app constants

---
+++ ExamPreparation/Exam30October2022/src/constants/index.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+exports.CONNECTION_URL = 'mongodb://localhost:27017/mind-blog';
+exports.SALT_ROUNDS = 13;
+exports.JWT_SECRET = 'THE BIGGEST SECRET EVER! >:)))';
+exports.COOKIE_NAME = 'AUTH_COOKIE_AUTH_YES';
