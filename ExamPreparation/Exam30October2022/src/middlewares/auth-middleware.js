a245c306748383c59eae9312c2acec118eede017
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-30 14:11:37 +0200

    Create auth middleware

---
+++ ExamPreparation/Exam30October2022/src/middlewares/auth-middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('../utils/jwt');
+
+const { COOKIE_NAME, JWT_SECRET } = require('../constants');
+
+exports.auth = (req, res, next) => {
+    const token = req.cookies[COOKIE_NAME];
+
+    if (token) {
+        jwt.verify(token, JWT_SECRET)
+            .then(decodedToken => {
+                req.user = decodedToken;
+                res.locals.user = decodedToken;
+                next();
+            })
+            .catch(err => {
+                res.clearCookie(COOKIE_NAME);
+                res.redirect('/auth/login')
+            });
+    } else {
+        next();
+    }
+}
